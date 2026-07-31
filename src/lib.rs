c8bde266a3a37efdd01478a5b6e9113231458184
maklean <yoelmakaya@gmail.com>
2026-07-31 05:06:23 -0400

    refactor: poof.

--- src/lib.rs
+++
@@ -1,7 +0,0 @@
-pub mod errors;
-pub mod module;
-pub mod decoder;
-pub mod definitions;
-pub mod instructions;
-mod validator;
-pub mod executor;
--- tests/common/mod.rs
+++
@@ -1,9 +0,0 @@
-mod types;
-mod conversion;
-mod spectest;
-mod utils;
-
-pub use types::{Manifest, Command, Action};
-pub use conversion::{SPEC_OUTPUT_DIR, convert_wasts};
-pub use spectest::{register_spectest, spectest_imports};
-pub use utils::{vals_match, resolve_module};
